522e8a724f674281b61f086bc49c5ec3e02db59a
Dbatr <bdanis2311@gmail.com>
2024-01-08 18:10:36 +0500

    Добавил модель в котором хранятся валюты

--- vacancies/admin.py
+++ vacancies/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from .models import Vacancy
+from .models import Currency
 
 # Register your models here.
 
 admin.site.register(Vacancy)
+admin.site.register(Currency)
