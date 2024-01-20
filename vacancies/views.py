ba22afa703e4ba477a809b15bb8b6456ef806f17
Dbatr <bdanis2311@gmail.com>
2024-01-20 18:50:28 +0500

    123

--- vacancies/views.py
+++ vacancies/views.py
@@ -30,7 +30,6 @@ def index(request):
     # Отображаем шаблон
     return render(request, 'vacancies/index.html')
 
-
 def execute_sql_query(query, key, cache_time=86400):
     cached_result = cache.get(key)
 
